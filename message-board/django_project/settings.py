71bbd309ddd57e348021e09e6b8fbe893a365e4b
MHolvani <michelle.holvani@snhu.edu>
2024-12-03 09:40:49 -0500

    Update settings.py

--- message-board/django_project/settings.py
+++ message-board/django_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g9r&_)+4c(xy3x=o%vcympwm!y5m32ys$muebts4w2woa1-ko9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
